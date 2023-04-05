af4b91341cab3636ecbd40dbc0964c25ea7cb3aa
hikurt <32220244+hikurt@users.noreply.github.com>
2023-04-05 13:57:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# section-20-multi-page-spa-router
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-hcixxe)
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div>Something</div>;
+}
+
+export default App;
